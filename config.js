78fee67bb7eab54d99a742961bc43ec1b3314950
Hallvord R. M. Steen <hsteen@mozilla.com>
2015-06-01 13:10:01 +0200

    default to finding local seraphConfig.json

--- config.js
+++ config.js
@@ -29,9 +29,9 @@ config.GOD_ADDRESS = getEnv(env.GOD_ADDRESS, "127.0.0.1");
 config.GOD_PORT = getEnv(env.GOD_PORT, 7331);
 config.GOD_BACK_CHANNEL_PORT = getEnv(env.GOD_BACK_CHANNEL_PORT, 3000);
 
-// SERAPH TODO its seraph singular and seraphim plural ! 
+// SERAPH TODO its seraph singular and seraphim plural !
 config.SERAPH_PORT = getEnv(env.SERAPH_PORT, 8421);
-config.SERAPH_CONFIG_PATH = getEnv(env.SERAPH_CONFIG_PATH, '/tmp/seraphConfig.json');
+config.SERAPH_CONFIG_PATH = getEnv(env.SERAPH_CONFIG_PATH, './seraphConfig.json');
 config.ANGEL_START_PORT = getEnv(env.ANGEL_START_PORT, 55550);
 config.ANGEL_PORT_COUNT = getEnv(env.ANGEL_PORT_COUNT, 5);
 try {
